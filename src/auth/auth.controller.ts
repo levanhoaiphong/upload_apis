844df4edf922c6ad60b72260574ad13d97efab34
Le Van Hoai Phong <77775917+levanhoaiphong@users.noreply.github.com>
2024-05-22 17:12:11 +0700

    Create Object Auth

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,15 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post("/login")
+  login(@Body() body){
+    return this.authService.login(body)
+  }
+  signUp(@Body() body){
+    return this.authService.signIn(body)
+  }
+}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class AuthService {
+    constructor(private JwtService: JwtService){}
+    prisma = new PrismaClient()
+    login(model) {
+        let {email, password} = model
+        let token = this.JwtService.sign({ data: "abc" }, { expiresIn: "5d", algorithm: "HS256", secret: "BI_MAT" })
+        return token
+    }
+    signIn(model) {
+        let { email, password } = model
+        return "OK"
+    }
+}
